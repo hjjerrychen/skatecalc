c74aecad06e4a3ef9a3b04bb36f139b0be079c40
Jerry Chen <chenhongjin2005@hotmail.com>
2018-11-11 12:38:33 -0500

    table test

--- script.js
+++ script.js
@@ -64,84 +64,120 @@ function setName(){
 function setType(node){
   if ($(node).parents(".nav-jmp").length){
     buffer.type = "jump";
+    $("#nav-sp-tab").addClass("disabled");
+    $("#nav-seq-tab").addClass("disabled");
   }
   else if ($(node).parents(".nav-sp").length){
     buffer.type = "spin";
+    $("#nav-jmp-tab").addClass("disabled");
+    $("#nav-seq-tab").addClass("disabled");
   }
   else {
     buffer.type = "seq";
+    $("#nav-jmp-tab").addClass("disabled");
+    $("#nav-sp-tab").addClass("disabled");
+    if (buffer.name == "ChSq"){
+    $("#nav-seq .setLOD button").addClass("disabled");
+    $("#nav-seq .setLOD button:eq(0)").removeClass("disabled");
+    $("#nav-seq .setLOD button:eq(2)").removeClass("disabled");
+    }
   }
 
-
-  elementDisplay.append(buffer.type);
+  //elementDisplay.append(buffer.type);
 }
 
 
 function setLOD(){
   buffer.lod = $(this).html();
-  elementDisplay.append(buffer.lod);
+  setType(this);
+  renderBufferedElement();
+//  elementDisplay.append(buffer.lod);
 }
 
 function setUr(){
   buffer.ur = !buffer.ur;
-  elementDisplay.append("<");
-
+  buffer.dg = false;
+  //elementDisplay.append("<");
+  setType(this);
+  renderBufferedElement();
 }
 
 function setDg(){
   buffer.dg = !buffer.dg;
-  elementDisplay.append("<<");
+  //elementDisplay.append("<<");
+  buffer.ur = false;
+  setType(this);
+  renderBufferedElement();
 }
 
 function setEdge(){
   buffer.edge = !buffer.edge;
-  elementDisplay.append("e");
+  //elementDisplay.append("e");
+  setType(this);
+  renderBufferedElement();
 }
 
 
 function setREP(){
   buffer.rep = !buffer.rep;
-  elementDisplay.append("+REP");
+  //elementDisplay.append("+REP");
+  setType(this);
+  renderBufferedElement();
 }
 
 function setBonus(){
   buffer.bonus = !buffer.bonus;
-  elementDisplay.append("x");
+  //elementDisplay.append("x");
+  setType(this);
+  renderBufferedElement();
 }
 
 function setInvalid(){
   buffer.invalid = !buffer.invalid;
-  elementDisplay.append("*");
+  //elementDisplay.append("*");
+  setType(this);
+  renderBufferedElement();
 }
 
 function setFly(){
   buffer.fly = !buffer.fly;
-  elementDisplay.append("F");
+  //elementDisplay.append("F");
+  setType(this);
+  renderBufferedElement();
 }
 
 function setSpinV(){
   buffer.spinV = !buffer.spinV;
-  elementDisplay.append("V");
+  //lementDisplay.append("V");
+  setType(this);
+  renderBufferedElement();
 }
 
 function setCOF(){
   buffer.cof = !buffer.cof;
-  elementDisplay.append("Co");
+  //elementDisplay.append("Co");
+  setType(this);
+  renderBufferedElement();
 }
 
 function setGOE(){
   buffer.goe = parseInt($(this).html());
-  elementDisplay.append(buffer.goe);
+  //elementDisplay.append(buffer.goe);
+  //setType(this);
+  renderBufferedElement();
 }
 
 function renderBufferedElement(){
   elementDisplay.html("");
   if(buffer.type === "jump"){
-    if (buffer.lod !== null){
+    if (buffer.lod != null && buffer.lod != 0){
       elementDisplay.append(buffer.lod);
     }
     if (buffer.name !== null){
       elementDisplay.append(buffer.name);
+    }
+    if (buffer.edge !== false){
+      elementDisplay.append("e");
     }
     if (buffer.ur !== false){
       elementDisplay.append("<");
@@ -149,22 +185,34 @@ function renderBufferedElement(){
     if (buffer.dg !== false){
       elementDisplay.append("<<");
     }
-    if (buffer.edge !== false){
-      elementDisplay.append("e");
+    if (buffer.invalid !== false){
+      elementDisplay.append("*");
     }
-    if (buffer.edge !== false){
-      elementDisplay.append("e");
-    }
+
     if (buffer.rep !== false){
       elementDisplay.append("+REP");
     }
+
+    if (buffer.bonus !== false){
+      elementDisplay.append(" x");
+    }
+
   }
   else if(buffer.type === "spin"){
+    if (buffer.fly !== false){
+      elementDisplay.append("F");
+    }
+    if (buffer.cof !== false){
+      elementDisplay.append("C");
+    }
     if (buffer.name !== null){
       elementDisplay.append(buffer.name);
     }
-    if (buffer.lod !== null){
+    if (buffer.lod != null && buffer.lod != 0){
       elementDisplay.append(buffer.lod);
+    }
+    if (buffer.spinV !== false){
+      elementDisplay.append("V");
     }
     if (buffer.invalid !== false){
       elementDisplay.append("*");
@@ -174,13 +222,14 @@ function renderBufferedElement(){
     if (buffer.name !== null){
       elementDisplay.append(buffer.name);
     }
-    if (buffer.lod !== null){
+    if (buffer.lod != null && buffer.lod != 0){
       elementDisplay.append(buffer.lod);
     }
     if (buffer.invalid !== false){
       elementDisplay.append("*");
     }
   }
+  $("#goeDisplay").html(buffer.goe);
 }
     //elementDisplay.html("test");
 
